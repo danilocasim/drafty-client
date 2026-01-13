ecdc28ceeb3346a3a8c60d0a7671679751bab0fa
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-13 17:43:39 +0800

    feat: fetch all public posts within homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,21 @@
+import { useContext, useEffect, useState } from "react";
 import style from "./Homepage.module.css";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blog/v1/post")
+      .then((response) => response.json())
+      .then((data) => setPosts(data.data));
+  }, [setPosts]);
+
   return (
-    <div className={style.homepageWrapper}>
-      <div>posts</div>
+    <div>
+      {posts &&
+        posts.map((post) => {
+          return <h1>{post.title}</h1>;
+        })}
     </div>
   );
 }
